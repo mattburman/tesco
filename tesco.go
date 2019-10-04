67d024bb9b3528ddcc91cc2f4a573a9201fa3095
Matt Burman <matt.burman@skybettingandgaming.com>
2019-10-04 15:48:34 +0100

    Return tesco URL from getProduct

--- tesco.go
+++ tesco.go
@@ -5,12 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"strconv"
 )
+
+var productf string = "https://www.tesco.com/groceries/en-GB/products/%v"
 
 func main() {
 	var productID int64
-	flag.Int64Var(&productID, "pid", 0, "https://www.tesco.com/groceries/en-GB/products/<productID>")
+	flag.Int64Var(&productID, "pid", 0, productf)
 	flag.Parse()
 	res, err := getProduct(productID)
 	if err != nil {
@@ -22,5 +23,13 @@ func main() {
 // getProduct returns the product data
 // or an error for parameter, network or request failures
 func getProduct(id int64) (string, error) {
-	return strconv.FormatInt(id, 10), nil
+	if id < 100000000 {
+		return "", fmt.Errorf("getProduct: %v is an invalid productID", id)
+	}
+
+	productURL := fmt.Sprintf(productf, id)
+
+	// resp, err := http.Get("")
+
+	return productURL, nil
 }
