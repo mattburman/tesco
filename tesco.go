c05e90be4d8b9b95c660213f10c3cf688d8a0d3c
Matt Burman <matt.burman@skybettingandgaming.com>
2019-10-04 15:22:00 +0100

    Initial setup of a getProduct method

--- tesco.go
+++ tesco.go
@@ -4,11 +4,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"strconv"
 )
 
 func main() {
 	var productID int64
 	flag.Int64Var(&productID, "pid", 0, "https://www.tesco.com/groceries/en-GB/products/<productID>")
 	flag.Parse()
-	fmt.Println(productID)
+	res, err := getProduct(productID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+}
+
+// getProduct returns the product data
+// or an error for parameter, network or request failures
+func getProduct(id int64) (string, error) {
+	return strconv.FormatInt(id, 10), nil
 }
