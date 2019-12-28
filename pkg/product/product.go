bbb64982bae716cc36706c7e6bcee3165b244e56
Matt Burman <matt@mattburman.com>
2019-12-28 23:16:10 +0000

    Add Product struct and constructor

--- pkg/product/product.go
+++ pkg/product/product.go
@@ -1,6 +1,7 @@
 package product
 
 import (
+	"crypto/sha1"
 	"database/sql"
 	"errors"
 	"fmt"
@@ -20,7 +21,154 @@ var (
 	productf          string         = "https://www.tesco.com/groceries/en-GB/products/%v"
 	dataRegexp        *regexp.Regexp = regexp.MustCompile(`data-props="({.*})"`)
 	invalidProductIDf string         = "%v is an invalid productID"
+	perGrams          *regexp.Regexp = regexp.MustCompile(`^Per (?P<grams>\d+)g$`)
+	perServingGrams   *regexp.Regexp = regexp.MustCompile(`.*\((?P<grams>\d+)g\).*`)
+	floatGrams        *regexp.Regexp = regexp.MustCompile(`(?P<grams>\d+(\.\d+)?)g`)
+	floatKcal         *regexp.Regexp = regexp.MustCompile(`\d+kJ / (?P<kcal>\d+)kcal`)
 )
+
+type Macros struct {
+	per     string
+	size    float64
+	carbs   float64
+	protein float64
+	fat     float64
+	kcal    float64
+}
+
+type Source struct {
+	url  string
+	id   string
+	name string
+}
+
+type Product struct {
+	name                            string
+	source                          Source
+	description                     []string
+	raw                             string
+	hashOfRawValueLastUsedToCompute string
+	perComp                         Macros
+	perServing                      Macros
+}
+
+func NewProduct(raw string, url string) (*Product, error) {
+	results := gjson.GetMany(
+		raw,
+		"pageTitle",
+		"product.description",
+		"product.details.nutritionInfo.#.name",
+		"product.details.nutritionInfo.#.perComp",
+		"product.details.nutritionInfo.#.perServing",
+	)
+	name := results[0].String()
+
+	id, err := URLToID(url)
+	if err != nil {
+		return nil, fmt.Errorf("unable to extract ID from %v: %v", url, err)
+	}
+
+	source := Source{url: url, id: id, name: "tesco"}
+
+	descriptionResults := results[1].Array()
+	description := make([]string, len(descriptionResults))
+	for i, result := range descriptionResults {
+		description[i] = result.String()
+	}
+
+	h := sha1.New()
+	h.Write([]byte(raw))
+	hashOfRawValueLastUsedToCompute := fmt.Sprintf("%x", h.Sum(nil))
+
+	//json.product.details.nutritionInfo[0].name = "Typical Values";
+	//json.product.details.nutritionInfo[1].name = "Energy";
+	//json.product.details.nutritionInfo[2].name = "Fat";
+	//json.product.details.nutritionInfo[3].name = "Saturates";
+	//json.product.details.nutritionInfo[4].name = "Carbohydrate";
+	//json.product.details.nutritionInfo[5].name = "Sugars";
+	//json.product.details.nutritionInfo[6].name = "Fibre";
+	//json.product.details.nutritionInfo[7].name = "Protein";
+	//json.product.details.nutritionInfo[8].name = "Salt";
+
+	nutrientNames := results[2].Array()
+	nutrientPerComps := results[3].Array()
+	nutrientPerServings := results[4].Array()
+
+	nutrientIndices := make(map[string]int)
+	for i, nutrient := range nutrientNames {
+		nutrientIndices[nutrient.String()] = i
+	}
+
+	perComp := Macros{}
+	perServing := Macros{}
+
+	i := nutrientIndices["Typical Values"]
+	perComp.per = nutrientPerComps[i].String()
+	perServing.per = nutrientPerServings[i].String()
+	match := perGrams.FindSubmatch([]byte(perComp.per))
+	if len(match) == 2 {
+		perComp.size, _ = strconv.ParseFloat(string(match[1]), 64)
+	}
+	match = perServingGrams.FindSubmatch([]byte(perServing.per))
+	if len(match) == 2 {
+		perServing.size, _ = strconv.ParseFloat(string(match[1]), 64)
+	}
+
+	i = nutrientIndices["Fat"]
+	match = floatGrams.FindSubmatch([]byte(nutrientPerComps[i].String()))
+	if len(match) > 1 {
+		perComp.fat, _ = strconv.ParseFloat(string(match[1]), 64)
+	}
+	match = floatGrams.FindSubmatch([]byte(nutrientPerServings[i].String()))
+	if len(match) > 1 {
+		perServing.fat, _ = strconv.ParseFloat(string(match[1]), 64)
+	}
+
+	i = nutrientIndices["Protein"]
+	match = floatGrams.FindSubmatch([]byte(nutrientPerComps[i].String()))
+	if len(match) > 1 {
+		perComp.protein, _ = strconv.ParseFloat(string(match[1]), 64)
+	}
+	match = floatGrams.FindSubmatch([]byte(nutrientPerServings[i].String()))
+	if len(match) > 1 {
+		perServing.protein, _ = strconv.ParseFloat(string(match[1]), 64)
+	}
+
+	i = nutrientIndices["Carbohydrate"]
+	match = floatGrams.FindSubmatch([]byte(nutrientPerComps[i].String()))
+	if len(match) > 1 {
+		perComp.carbs, _ = strconv.ParseFloat(string(match[1]), 64)
+	}
+	match = floatGrams.FindSubmatch([]byte(nutrientPerServings[i].String()))
+	if len(match) > 1 {
+		perServing.carbs, _ = strconv.ParseFloat(string(match[1]), 64)
+	}
+
+	i = nutrientIndices["Energy"]
+	match = floatKcal.FindSubmatch([]byte(nutrientPerComps[i].String()))
+	fmt.Println(string(match[1]))
+	if len(match) > 1 {
+		perComp.kcal, _ = strconv.ParseFloat(string(match[1]), 64)
+	}
+	match = floatKcal.FindSubmatch([]byte(nutrientPerServings[i].String()))
+	if len(match) > 1 {
+		perServing.kcal, _ = strconv.ParseFloat(string(match[1]), 64)
+	}
+
+	product := Product{
+		name:                            name,
+		source:                          source,
+		description:                     description,
+		raw:                             raw,
+		hashOfRawValueLastUsedToCompute: hashOfRawValueLastUsedToCompute,
+		perComp:                         perComp,
+		perServing:                      perServing,
+	}
+
+	fmt.Println(product)
+	fmt.Println(product.description)
+	return &product, nil
+}
 
 // GetProduct returns the product data
 // or an error for parameter, network or request failures
@@ -129,4 +277,3 @@ func URLToID(url string) (string, error) {
 	}
 	return string(match[1]), nil
 }
-
